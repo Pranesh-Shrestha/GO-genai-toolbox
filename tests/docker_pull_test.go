1be7695f1175c24b8b93556c39c5f2abcd4227e3
Pranesh-Shrestha <pranesh.shrestha@tekvortex.com>
2025-08-27 08:03:02 +0545

    Create docker_pull_test.go

---
+++ tests/docker_pull_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os/exec"
+	"testing"
+)
+
+func TestDockerPull(t *testing.T) {
+	image := "testcontainers/ryuk:0.12.0"
+
+	cmd := exec.Command("docker", "pull", image)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("failed to pull docker image %s: %v\nOutput: %s", image, err, string(output))
+	}
+
+	t.Logf("successfully pulled docker image: %s\nOutput: %s", image, string(output))
+}
